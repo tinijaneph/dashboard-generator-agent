b8af10abd89efac512cecfec83a5c3d3c7ec97a2
tinijaneph <janetiniph@gmail.com>
2026-03-17 02:25:43 -0400

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'deep-navy': '#0a1929',
+        'dark-blue': '#0d2137',
+        'medium-blue': '#2e5c8a',
+        'light-blue': '#64b5f6',
+        'pale-blue': '#b3d4f5',
+        'muted-blue': '#8aaec9',
+      }
+    },
+  },
+  plugins: [],
+}
